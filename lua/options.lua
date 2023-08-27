5cd72642109901d315826e742a5d3473ccf49e3d
timohahaa <timabd77@gmail.com>
2023-08-27 23:34:10 +0300

    v1 fixes

--- lua/options.lua
+++ lua/options.lua
@@ -12,13 +12,31 @@ opt.termguicolors = true
 opt.ignorecase = true
 opt.smartcase = true
 
+opt.wrap = true
+opt.expandtab = true
+-- configure indentation
+opt.tabstop = 4 --for everything except see lower
+opt.smartindent = true
+opt.shiftwidth = 4
+
+vim.cmd([[
+augroup tabGroup
+autocmd FileType lua, yaml setlocal shiftwidth=2 tabstop=2
+augroup end
+]])
+--local tabGroup = vim.api.nvim_create_augroup('tabGroup', { clear = true })
+--vim.api.nvim_create_autocmd({ "FileType yaml,lua" }, {
+--	group = tabGroup,
+--	command = "setlocal shiftwidth=2 tabstop=2"
+--})
+
+
 
 -- autocomands
-local fmtGroup = vim.api.nvim_create_augroup('fmtGroup', {clear = true})
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-	group = fmtGroup,
-	callback = function()
-		vim.lsp.buf.format({ async = true })
-	end,
+local fmtGroup = vim.api.nvim_create_augroup('fmtGroup', { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = fmtGroup,
+  callback = function()
+    vim.lsp.buf.format({ async = true })
+  end,
 })
-
