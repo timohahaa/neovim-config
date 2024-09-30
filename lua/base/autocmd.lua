fa564972f99722caa5e00a0cc2de8448d741b997
Timofey <timabd77@gmail.com>
2024-09-30 21:18:23 +0300

    change shiftwidth and tabstop for jsx files

--- lua/base/autocmd.lua
+++ lua/base/autocmd.lua
@@ -2,7 +2,7 @@
 
 -- configure filetypes, than need to have a different indent
 vim.cmd([[augroup TabGroup
-autocmd FileType lua,yaml,vue,html,css :setlocal shiftwidth=2 tabstop=2 softtabstop=2
+autocmd FileType lua,yaml,vue,html,css,javascriptreact,typescriptreact :setlocal shiftwidth=2 tabstop=2 softtabstop=2
 augroup end
 ]])
 
